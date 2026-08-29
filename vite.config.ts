695ab099e224037acdee5616b2c3661efacb49d5
yudaprama <yudhaprama@icloud.com>
2026-08-29 21:29:26 +0700

    optimize build size

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
     outDir: path.resolve(__dirname, "dist"),
     emptyOutDir: true,
     rollupOptions: {
-      external: [/^shiki(\/.*)?$/],
+      external: [/^shiki(\/.*)?$/, /^mermaid(\/.*)?$/, /^katex(\/.*)?$/, /^rehype-katex(\/.*)?$/],
     },
   },
 
